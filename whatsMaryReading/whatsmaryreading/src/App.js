448bab258479c4b3d47ca4d91ebf25d43e810ec2
Mary Pletcher <mcpletcher@vt.edu>
2025-05-23 20:51:53 -0400

    can get json of storygraph library

--- whatsMaryReading/whatsmaryreading/src/App.js
+++ whatsMaryReading/whatsmaryreading/src/App.js
@@ -1,34 +1,52 @@
-//import './App.css';
-import { useState } from 'react';
-
-
-function MyButton() {
-  const [count, setCount] = useState(0);
-
-  function handleClick() {
-    setCount(count + 1);
-  }
-
-  return (
-    <button onClick={handleClick}>
-      Clicked {count} times
-    </button>
-  );
-
-}
-
-
-export default function MyApp() {
-  return (
-    <div>
-      <h1>Counters that update separately</h1>
-      <MyButton />
-      <MyButton />
-    </div>
-  );
-}
+import './App.css';
+import { useEffect, useState } from 'react';
+import Papa from "papaparse";
 
 //hooks
+
+//https://pypi.org/project/storygraph-api/
+
+//reading storygraph output
+const CsvReader = () => {
+  //where csv is stored
+  const [csvData, setCsvData] = useState([]);
+//useEffect runs when component loads
+  useEffect(() => {
+    console.log("Component mounted. Starting to fetch CSV...");
+    //this fetches from the public folder
+    fetch("46da598b0e9b803785d252058ba6021432e88c7e500f2555dbccf856f15cd623.csv")
+      .then(response => {
+        console.log("fetch response: ", response);
+        return response.text()
+      }) //convert it to plain text
+      .then(csvText => {
+        //see raw CSV
+        console.log("CSV text received:");
+        console.log(csvText); // See raw CSV text
+
+        Papa.parse(csvText, {
+          header: true, // if your CSV has headers
+          skipEmptyLines: true,
+          complete: (results) => {
+            setCsvData(results.data); //results.data is array of row objects
+          },
+        });
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>What's Mary Reading?</h1>
+      <ul>
+        {csvData.map((row, index) => (
+          <li key={index}>{JSON.stringify(row)}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default CsvReader;
 
 
 
